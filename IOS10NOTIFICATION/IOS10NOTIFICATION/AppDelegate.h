e322d79f45a4c7c79d35be2cad9988f62a4a820c
贺嘉炜 <hjw6160602@163.com>
2016-11-22 18:08:25 +0800

    first commit

---
+++ IOS10NOTIFICATION/IOS10NOTIFICATION/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS10NOTIFICATION
+//
+//  Created by 贺嘉炜 on 2016/11/22.
+//  Copyright © 2016年 贺嘉炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS10NOTIFICATION/IOS10NOTIFICATION/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS10NOTIFICATION
+//
+//  Created by 贺嘉炜 on 2016/11/22.
+//  Copyright © 2016年 贺嘉炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
