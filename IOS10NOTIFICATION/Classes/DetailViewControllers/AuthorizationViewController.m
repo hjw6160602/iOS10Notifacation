f819323e39a7fdd60069da3bcb11521055cd9484
贺嘉炜 <hjw6160602@163.com>
2016-11-23 15:33:17 +0800

    commit

--- IOS10NOTIFICATION/Classes/DetailViewControllers/AuthorizationViewController.m
+++ IOS10NOTIFICATION/Classes/DetailViewControllers/AuthorizationViewController.m
@@ -10,6 +10,23 @@
 
 @interface AuthorizationViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *deviceTokenLabel;
+
+@property (weak, nonatomic) IBOutlet UIStackView *settingsView;
+
+@property (weak, nonatomic) IBOutlet UILabel *notificationCenterSettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *soundSettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *badgeSettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *lockScreenSettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *alertSettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *carPlaySettingLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *alertStyleSettingLabel;
 @end
 
 @implementation AuthorizationViewController
@@ -19,19 +36,9 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)requestBtnOnClick:(id)sender {
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
